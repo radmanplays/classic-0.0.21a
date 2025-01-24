cf821947f898d8db41d48b7f2c437fcf460a7db9
WtmcX <136674304+WtmcX@users.noreply.github.com>
2025-01-24 21:10:21 +0300

    so

---
+++ src/main/java/com/demez/minecraft/Client/Client.java
@@ -0,0 +1,14 @@
+package com.demez.minecraft.Client;
+
+import com.mojang.minecraft.Minecraft;
+
+public class Client {
+    public static void main(String[] args) {
+        // Create an instance of the Minecraft class
+        Minecraft minecraft = new Minecraft(null, 800, 600, false);
+
+        // Start the Minecraft instance in a separate thread
+        Thread gameThread = new Thread(minecraft);
+        gameThread.start();
+    }
+}
