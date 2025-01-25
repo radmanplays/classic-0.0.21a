8655eff09e51ec6c8101bf24743a50761f790918
radmanplays <95340057+radmanplays@users.noreply.github.com>
2025-01-25 18:24:12 +0330

    fix bug

--- src/teavm/java/me/radmanplays/Util.java
+++ src/teavm/java/me/radmanplays/Util.java
@@ -1,9 +1,8 @@
 package me.radmanplays;
 
+import org.teavm.jso.JSBody;
+
 public class Util {
-	public static void alert(String message, String title) {
-		JOptionPane.showMessageDialog((Component)null, message, title, 0);
-	}
     @JSBody(params = { "message", "title" }, script = "alert(title + '\n\n' + message)")
     public static native void alert(String message, String title);
 }
